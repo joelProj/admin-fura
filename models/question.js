eb2e24496bf318c931ed44b1dad40ba2e414d829
Joel <joel@tvrbo.pro>
2018-10-23 19:58:51 +0200

    src prepared remains api backend

--- models/question.js
+++ models/question.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.model('Question', mongoose.Schema({
 
+    questionnaire: { type: String, required: true },
     id_fura: { type: String, required: true },
-    text: { type: String, required: true },
     group: { type: String, required: true },
+    text: { type: String, required: true },
     options: {
         type: [{ type: String, required: true }],
         default: []
 	},
-	questionnaire: { type: String, required: true },
     date: { type: Date, default: Date.now }
 
 },
--- src/entities/questions.js
+++ src/entities/questions.js
@@ -1,5 +1,3 @@
-var removeDiacritics = require('../../lib/util.js').removeDiacritics;
-
 module.exports = function (nga, admin) {
 
 		var questions = admin.getEntity('questions');
@@ -9,153 +7,128 @@ module.exports = function (nga, admin) {
 		questions.listView()
 		.title('Questions')
 		.fields([
-				nga.field('name')
-					.map(function(v){ return v || '(No name)'})
-					.isDetailLink(true),
-				nga.field('lastName')
-					.label('Last Name')
-					.isDetailLink(true),
-				nga.field('type', 'choice')
-						.choices([
-								{label: 'Retail', value: 'Retail'},
-								{label: 'Wholesale', value: 'Wholesale'}
-						])
-						.cssClasses(function(entry) { // add custom CSS classes to inputs and columns
-								if(!entry || !entry.values) return '';
-								else if(!entry.values.type) return 'bg-danger';
-								else if (entry.values.type == 'Retail') return 'bg-success';
-								else return 'bg-info';
-						}),
-				nga.field('registered', 'date').label('Registered').format('dd/MM/yyyy')
+			nga.field('questionnaire').label('Questionnaire').isDetailLink(true),
+			nga.field('id_fura').label("ID").isDetailLink(true),
+			nga.field('group').label('Group').isDetailLink(true),
+			nga.field('text').label('Question'),
+			nga.field('date', 'date').label('Created').format('dd/MM/yyyy')
 		])
-		.sortField('lastName')
+		.sortField('id_fura')
 		.sortDir('ASC')
-		.listActions(['edit'])
-		.batchActions([
-			'<questions-mark-retail selection="selection"></questions-mark-retail>',
-			'<questions-mark-wholesale selection="selection"></questions-mark-wholesale>',
-			'delete'
-		])
+		.listActions(['show','edit'])
 		.filters([
-			nga.field('type', 'choice')
-					.choices([
-							{label: 'Retail', value: 'Retail'},
-							{label: 'Wholesale', value: 'Wholesale'}
-					])
-		])
-		.exportOptions({
-			quotes: true,
-			delimiter: ';'
-		})
-		.exportFields([
-			nga.field('name').map(removeDiacritics),
-			nga.field('lastName').label('Last Name').map(removeDiacritics),
-			nga.field('type', 'choice').map(function(value){
-				switch(value){
-					case 'Retail': return 'Retail';
-					case 'Wholesale': return 'Wholesale';
-				}
-				return value;
-			}),
-			nga.field('email'),
-			nga.field('phone'),
-			nga.field('delivery.address').map(removeDiacritics),
-			nga.field('delivery.city').map(removeDiacritics),
-			nga.field('delivery.zip'),
-			// nga.field('delivery.region', 'reference')
-			// 	.targetEntity(admin.getEntity('regions'))
-			// 	.targetField(nga.field('name').map(removeDiacritics)),
-			// nga.field('delivery.country', 'reference')
-			// 	.targetEntity(admin.getEntity('countries'))
-			// 	.targetField(nga.field('name').map(removeDiacritics)),
-			nga.field('billing.nif'),
-			nga.field('billing.name').map(removeDiacritics),
-			nga.field('billing.address').map(removeDiacritics),
-			nga.field('billing.city').map(removeDiacritics),
-			nga.field('billing.zip'),
-			// nga.field('billing.country', 'reference')
-			// 	.label('Pais (facturacion)')
-			// 	.targetEntity(admin.getEntity('countries'))
-			// 	.targetField(nga.field('name').map(removeDiacritics)),
-			nga.field('registered', 'date').format('dd/MM/yyyy')
+			nga.field('questionnaire'),
+			nga.field('group')
 		]);
 
+		questions.showView()
+		.title('Question')
+		.fields([
+			nga.field('questionnaire').label('Questionnaire'),
+			nga.field('id_fura').label("ID"),
+			nga.field('group').label('Group'),
+			nga.field('text').label('Question'),
+			nga.field('options', 'embedded_list').label('Options'),
+			nga.field('date', 'date').label('Created').format('dd/MM/yyyy')
+		])
+		.actions(['edit']);
+
 		questions.editionView()
-				.title('Question')
-				.actions([
-					'<question-mark-retail question="entry"></question-mark-retail>',
-					'<question-mark-wholesale question="entry"></question-mark-wholesale>',
-					'list'
-				])
-				.fields([
-						nga.field('name'),
-						nga.field('lastName').label('Last Name'),
-						nga.field('type', 'choice')
-							.choices([
-								{label: 'Retail', value: 'Retail'},
-								{label: 'Wholesale', value: 'Wholesale'}
-							]),
-						nga.field('email'),
-						nga.field('phone'),
-						nga.field('delivery.address'),
-						nga.field('delivery.city'),
-						nga.field('delivery.zip'),
-						nga.field('billing.nif'),
-						nga.field('billing.name'),
-						nga.field('billing.address'),
-						nga.field('billing.city'),
-						nga.field('billing.zip'),
-						nga.field('registered', 'date')
-							.format('dd/MM/yyyy')
-							.editable(false),
+		.title('Question')
+		.fields([
+			nga.field('questionnaire').label('Questionnaire'),
+			nga.field('id_fura').label("ID"),
+			nga.field('group').label('Group'),
+			nga.field('text').label('Question'),
+			nga.field('options', 'embedded_list').label('Options')
+		]);
 
-						// nga.field('purchases', 'referenced_list')
-						// 		.label('Ventas')
-						// 		.targetEntity(admin.getEntity('purchases'))
-						// 		.targetReferenceField('question')
-						// 		.targetFields([
-						// 			nga.field('reference').label('Localizador').isDetailLink(true),
-						// 			nga.field('date', 'date').label('Fecha').isDetailLink(true).format('dd/MM/yyyy'),
-						// 			nga.field('status', 'choice').label('Estado')
-						// 				.choices([
-						// 					{label: 'Pago pendiente', value: 'Pending'},
-						// 					{label: 'Pagada', value: 'Paid'},
-						// 					{label: 'Remesa', value: 'Remitted'}
-						// 				])
-						// 				.cssClasses(function(entry) {
-						// 						if(!entry) return '';
-						// 						else if (entry.values.status == 'Pending') return 'bg-warning';
-						// 						else if (entry.values.status == 'Remitted') return 'bg-info';
-						// 						else if (entry.values.status == 'Paid') return 'bg-success';
-						// 						else return 'bg-danger';
-						// 				})
-						// 		])
-						// 		.perPage(10)
-						// 		.sortField('date')
-						// 		.sortDir('DESC')
-				]);
+		questions.creationView()
+		.title('Question')
+		.fields([
+			nga.field('questionnaire').label('Questionnaire'),
+			nga.field('id_fura').label("ID"),
+			nga.field('group').label('Group'),
+			nga.field('text').label('Question'),
+			nga.field('options', 'embedded_list').label('Options')
+		]);
 
-				questions.creationView()
-						.title('Question')
-						.fields([
-								nga.field('name'),
-								nga.field('lastName').label('Last Name'),
-								nga.field('type', 'choice')
-									.choices([
-										{label: 'Retail', value: 'Retail'},
-										{label: 'Wholesale', value: 'Wholesale'}
-									]),
-								nga.field('email'),
-								nga.field('phone'),
-								nga.field('delivery.address'),
-								nga.field('delivery.city'),
-								nga.field('delivery.zip'),
-								nga.field('billing.nif'),
-								nga.field('billing.name'),
-								nga.field('billing.address'),
-								nga.field('billing.city'),
-								nga.field('billing.zip')
-						]);
+		// questions.editionView()
+		// 		.title('Question')
+		// 		.actions([
+		// 			'<question-mark-retail question="entry"></question-mark-retail>',
+		// 			'<question-mark-wholesale question="entry"></question-mark-wholesale>',
+		// 			'list'
+		// 		])
+		// 		.fields([
+		// 				nga.field('name'),
+		// 				nga.field('lastName').label('Last Name'),
+		// 				nga.field('type', 'choice')
+		// 					.choices([
+		// 						{label: 'Retail', value: 'Retail'},
+		// 						{label: 'Wholesale', value: 'Wholesale'}
+		// 					]),
+		// 				nga.field('email'),
+		// 				nga.field('phone'),
+		// 				nga.field('delivery.address'),
+		// 				nga.field('delivery.city'),
+		// 				nga.field('delivery.zip'),
+		// 				nga.field('billing.nif'),
+		// 				nga.field('billing.name'),
+		// 				nga.field('billing.address'),
+		// 				nga.field('billing.city'),
+		// 				nga.field('billing.zip'),
+		// 				nga.field('registered', 'date')
+		// 					.format('dd/MM/yyyy')
+		// 					.editable(false),
+
+		// 				// nga.field('purchases', 'referenced_list')
+		// 				// 		.label('Ventas')
+		// 				// 		.targetEntity(admin.getEntity('purchases'))
+		// 				// 		.targetReferenceField('question')
+		// 				// 		.targetFields([
+		// 				// 			nga.field('reference').label('Localizador').isDetailLink(true),
+		// 				// 			nga.field('date', 'date').label('Fecha').isDetailLink(true).format('dd/MM/yyyy'),
+		// 				// 			nga.field('status', 'choice').label('Estado')
+		// 				// 				.choices([
+		// 				// 					{label: 'Pago pendiente', value: 'Pending'},
+		// 				// 					{label: 'Pagada', value: 'Paid'},
+		// 				// 					{label: 'Remesa', value: 'Remitted'}
+		// 				// 				])
+		// 				// 				.cssClasses(function(entry) {
+		// 				// 						if(!entry) return '';
+		// 				// 						else if (entry.values.status == 'Pending') return 'bg-warning';
+		// 				// 						else if (entry.values.status == 'Remitted') return 'bg-info';
+		// 				// 						else if (entry.values.status == 'Paid') return 'bg-success';
+		// 				// 						else return 'bg-danger';
+		// 				// 				})
+		// 				// 		])
+		// 				// 		.perPage(10)
+		// 				// 		.sortField('date')
+		// 				// 		.sortDir('DESC')
+		// 		]);
+
+		// 		questions.creationView()
+		// 				.title('Question')
+		// 				.fields([
+		// 						nga.field('name'),
+		// 						nga.field('lastName').label('Last Name'),
+		// 						nga.field('type', 'choice')
+		// 							.choices([
+		// 								{label: 'Retail', value: 'Retail'},
+		// 								{label: 'Wholesale', value: 'Wholesale'}
+		// 							]),
+		// 						nga.field('email'),
+		// 						nga.field('phone'),
+		// 						nga.field('delivery.address'),
+		// 						nga.field('delivery.city'),
+		// 						nga.field('delivery.zip'),
+		// 						nga.field('billing.nif'),
+		// 						nga.field('billing.name'),
+		// 						nga.field('billing.address'),
+		// 						nga.field('billing.city'),
+		// 						nga.field('billing.zip')
+		// 				]);
 
 
 
